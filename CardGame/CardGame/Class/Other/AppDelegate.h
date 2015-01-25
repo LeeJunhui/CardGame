d342bb5937c4e4e5827c22185750bd779f57e3e4
LeeJunhui <84416097@qq.com>
2015-01-25 20:30:19 +0800

    项目搭建

---
+++ CardGame/CardGame/Class/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardGame
+//
+//  Created by LeeJunHui on 15/1/25.
+//  Copyright (c) 2015年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
